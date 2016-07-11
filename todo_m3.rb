b956ab91b3664583d7595c6f8a89e044a964ab6d
An Tran <tranminhan.my@gmail.com>
2016-07-11 11:05:42 +0700

    milestone 3

---
+++ todo_m3.rb
@@ -0,0 +1,35 @@
+class Todo 
+	def initialize
+		@title = "Todo"
+		@items = ["Decide supervisor and driver roles", "Implement milestone1"]
+	end
+end
+
+class HtmlTodo < Todo
+	def display
+		items_in_html = @items.map { |e| "      <li>#{e}</li>" } .join("\n")
+		puts "<html>
+  <head>
+    <title>#{@title}</title>
+  </head>
+  <body>
+    <ul>
+#{items_in_html}
+    </ul>
+  <body>
+</html>"
+	end
+end
+
+class PlainTodo < Todo
+	def display
+		items_in_plain = @items.map { |e| " - #{e}" } .join("\n")
+		puts "*** #{@title} ***
+#{items_in_plain}"
+	end
+end
+
+todo = HtmlTodo.new
+todo.display
+todo = PlainTodo.new
+todo.display
